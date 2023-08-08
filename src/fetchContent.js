d39fc7bee2849f67258e2ec2cb807844eb907018
Andrii Kharchenko <kh.andrew16@gmail.com>
2023-08-08 23:52:50 +0300

    with btn LoadMore

--- src/fetchContent.js
+++ src/fetchContent.js
@@ -1,4 +1,6 @@
-export function serviceContent(query) {
+import axios from "axios";
+
+export async function serviceContent(query, page, perPage) {
     const API_KEY = '38685077-7183c86b95211b39352b290b2';
     const BASE_URL = 'https://pixabay.com/api/';
 
@@ -6,22 +8,12 @@ export function serviceContent(query) {
         key: API_KEY,
         image_type: 'photo',
         orientation: 'horizontal',
-        safesearch: 'true'
+        safesearch: 'true',
     })
 
-    return fetch(`${BASE_URL}?${params}&q=${query}`)
-        .then((response) => {
-            if (!response.ok) {
-                throw new Error(response.statusText)
-            }
-            return response.json();
-        
-        })
-    
-    // axios
-    //     .get(`${BASE_URL}?${params}`)
-    //     .then(response => {
-    //         console.log(response.data);
-           
-    //     })
-}
+    const response = await axios.get(`${BASE_URL}?${params}&q=${query}&page=${page}&per_page=${perPage}`)
+    console.log(response.data);
+    // console.log(response.data.hits);
+    return response.data  
+         
+}; 
--- src/index.js
+++ src/index.js
@@ -1,25 +1,56 @@
-import axios from "axios";
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import {serviceContent} from "./fetchContent";
 
 // Елементи розмітки
 const elements = {
     form: document.querySelector('.search-form'),
-    gallery:document.querySelector('.gallery')
+    gallery: document.querySelector('.gallery'),
+    loadMore:document.querySelector('.load-more')
 };
+// Об'єкт параметрів для Notify
+const paramsForNotify = {
+    position: 'center-center',
+        timeout: 3000,
+        width: '500px',
+        fontSize: '20px'
+}
+
+let page = 1;  //Поточна сторінка - перша
+const perPage = 40  // К-сть елементів на сторінці.
+let query = "";
 
 elements.form.addEventListener("submit", onClickSend)
 
 function onClickSend(evt) {
     evt.preventDefault();
-    const query = evt.currentTarget.elements.searchQuery.value.trim()
-    serviceContent(query)
-    .then((data) => {
-    console.log(data.hits);
+    query = evt.currentTarget.elements.searchQuery.value.trim()
+// Перевірка чи поле пошуку не пусте
+    if (query === "") {
+        Notify.failure('Fill the search field', paramsForNotify);
+        return;
+    }
+// Робимо запит на бекенд
+    serviceContent(query, page, perPage)
+        .then(data => {
+            if (data.totalHits === 0) {
+                Notify.failure('Sorry, there are no images matching your search query. Please try again.', paramsForNotify);
+                return;
+                
+            } else {
+                const countPages = Math.ceil(data.totalHits / perPage);
+                const markup = createMarkup(data.hits);
+                elements.gallery.insertAdjacentHTML('beforeend', markup);
+                // Перевіряємо кількість сторінок і якщо вона не одна, то показуємо кнопку LoadMore
+                if (page !== countPages) {
+                    elements.loadMore.hidden = false;
+                }
+            }
+        // .catch(onFetchError);
     })
-
+    
 }
 
-
+elements.loadMore.addEventListener('click', onLoad);
 
 function createMarkup(arr)  {
     return arr.map(({ webformatURL, largeImageURL, tags, likes, views, comments, downloads }) => `
@@ -44,3 +75,20 @@ function createMarkup(arr)  {
     </a>
     `).join("");
 }   
+
+function onLoad() {
+    page += 1;
+    serviceContent(query, page, perPage)
+    .then(data => {
+        const markup = createMarkup(data.hits);
+        elements.gallery.insertAdjacentHTML('beforeend', markup);
+        // Перевірка чи ми знаходимось на останній сторінці колекції
+        if (page*perPage >= data.totalHits) {
+            elements.loadMore.hidden = true;
+            Notify.info("We're sorry, but you've reached the end of search results.", paramsForNotify);
+            elements.loadMore.removeEventListener('click', onLoad);
+        }
+        // .catch(onFetchError);
+    })
+}
+    
